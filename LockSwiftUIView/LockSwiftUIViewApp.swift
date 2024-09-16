5ea69d39007c06d86e25b0b711223158ba260f4c
muhammeteminayhan <aemin8343@gmail.com>
2024-09-16 17:21:53 +0300

    Initial Commit

---
+++ LockSwiftUIView/LockSwiftUIViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockSwiftUIViewApp.swift
+//  LockSwiftUIView
+//
+//  Created by Muhammet Emin AYHAN on 16.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct LockSwiftUIViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
